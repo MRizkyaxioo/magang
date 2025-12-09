c93282ce19f3e26cbafa6a07e5eb2f0d05f6f982
MRizkyaioo <14mrizky.rizky@gmail.com>
2025-12-09 20:37:44 +0800

    fourteenth.2 commit (perbaikan fitur cetak pdf)

--- resources/views/admin/statistik.blade.php
+++ resources/views/admin/statistik.blade.php
@@ -6,21 +6,6 @@
     body { font-family: sans-serif; }
     h2 { text-align: center; }
 
-    .bar-container { margin-top: 30px; }
-
-    .bar {
-        height: 25px;
-        margin-bottom: 15px;
-        color: white;
-        padding-left: 10px;
-        line-height: 25px;
-    }
-
-    .pending { background: orange; width: {{ $statistik['pending'] * 20 }}px; }
-    .sedang  { background: blue;   width: {{ $statistik['sedang']  * 20 }}px; }
-    .selesai { background: green;  width: {{ $statistik['selesai'] * 20 }}px; }
-    .ditolak { background: red;    width: {{ $statistik['ditolak'] * 20 }}px; }
-
     table {
         margin-top: 30px;
         width: 100%;
@@ -37,13 +22,6 @@
 <body>
 
 <h2>Statistik Pengaduan Bulan {{ $bulanLabel }}</h2>
-
-<div class="bar-container">
-    <div class="bar pending">Pending: {{ $statistik['pending'] }}</div>
-    <div class="bar sedang">Sedang Dikerjakan: {{ $statistik['sedang'] }}</div>
-    <div class="bar selesai">Selesai: {{ $statistik['selesai'] }}</div>
-    <div class="bar ditolak">Ditolak: {{ $statistik['ditolak'] }}</div>
-</div>
 
 <table>
     <tr>
